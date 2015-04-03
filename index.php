b8406ea3b0135b2cb650e9a5b74e3a1c4cbb14a0
Zeki <zeki-san@hotmail.com>
2015-04-03 14:11:45 -0300

    Commit inicial.

---
+++ index.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<title></title>
+</head>
+<body>
+  <table class="table">
+    <thead>
+      <th colspan="4">10ª Rodada</th>
+    </thead>
+    <tbody>
+      <tr>
+        <td>
+          <table>
+            <tr>
+              <td  colspan="5" align="center">qua - 11/03 - 17:00</td>
+            </tr>
+            <tr>
+              <td>
+                <a href="http://www.sercaxias.com.br/" target="_blank"><img src="img/caxias.png"></a>
+              </td>
+              <td align="left"  width="100px"><span>Caxias</span></td>
+              <td>
+                <input type="text" size="1">
+                x
+                <input type="text" size="1">
+              </td>
+              <td align="right"  width="100px">
+                <span>Lajeadense</span>
+              </td>
+              <td>
+                <img src="img/lajeadense.png"> 
+              </td>
+            </tr>
+          </table>
+        </td>
+        <td>
+          <table>
+            <tr>
+              <td  colspan="5" align="center">qua - 11/03 - 19:30</td>
+            </tr>
+            <tr>
+              <td>
+                <img src="img/inter.png">
+              </td>
+              <td  align="left"  width="120px"><span>Internacional</span></td>
+              <td>
+                <input type="text" size="1">
+                x
+                <input type="text" size="1">
+              </td>
+              <td align="right"  width="110px">
+                <span>Aimoré</span>
+              </td>
+              <td>
+                <img src="img/aimore.png"> 
+              </td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <table>
+            <tr>
+              <td  colspan="5" align="center">qua - 11/03 - 20:30</td>
+            </tr>
+            <tr>
+              <td>
+                <img src="img/saopaulo.png">
+              </td>
+              <td  align="left"  width="100px"><span>São Paulo</span></td>
+              <td>
+                <input type="text" size="1">
+                x
+                <input type="text" size="1">
+              </td>
+              <td  align="right"  width="100px">
+                <span>São José</span>
+              </td>
+              <td>
+                <img src="img/saojose.png"> 
+              </td>
+            </tr>
+          </table>
+        </td>
+        <td>
+          <table>
+            <tr>
+              <td  colspan="5" align="center">qua - 11/03 - 20:30</td>
+            </tr>
+            <tr>
+              <td>
+                <img src="img/novohamburgo.png">
+              </td>
+              <td align="left"  width="120px"><span>Novo Hamburgo</span></td>
+              <td>
+                <input type="text" size="1">
+                x
+                <input type="text" size="1">
+              </td>
+              <td align="right"  width="110px">
+                <span>Cruzeiro</span>
+              </td>
+              <td>
+                <img src="img/cruzeiro.png"> 
+              </td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <table>
+            <tr>
+              <td  colspan="5" align="center">qua - 11/03 - 20:30</td>
+            </tr>
+            <tr>
+              <td>
+                <img src="img/veranopolis.png">
+              </td>
+              <td  align="left"  width="100px"><span>Veranópolis</span></td>
+              <td>
+                <input type="text" size="1">
+                x
+                <input type="text" size="1">
+              </td>
+              <td  align="right"  width="100px">
+                <span>Avenida</span>
+              </td>
+              <td>
+                <img src="img/avenida.png"> 
+              </td>
+            </tr>
+          </table>
+        </td>
+        <td>
+          <table>
+            <tr>
+              <td  colspan="5" align="center">qua - 11/03 - 22:00</td>
+            </tr>
+            <tr>
+              <td>
+                <img src="img/ypiranga.png">
+              </td>
+              <td align="left"  width="120px"><span>Ypiranga</span></td>
+              <td>
+                <input type="text" size="1">
+                x
+                <input type="text" size="1">
+              </td>
+              <td align="right"  width="110px">
+                <span>Grêmio</span>
+              </td>
+              <td>
+                <img src="img/gremio.png"> 
+              </td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <table>
+            <tr>
+              <td  colspan="5" align="center">qui - 12/03 - 16:00</td>
+            </tr>
+            <tr>
+              <td>
+                <img src="img/uniao.png">
+              </td>
+              <td  align="left"  width="100px"><span>União</span></td>
+              <td>
+                <input type="text" size="1">
+                x
+                <input type="text" size="1">
+              </td>
+              <td  align="right"  width="100px">
+                <span>Juventude</span>
+              </td>
+              <td>
+                <img src="img/juventude.png"> 
+              </td>
+            </tr>
+          </table>
+        </td>
+        <td>
+          <table>
+            <tr>
+              <td  colspan="5" align="center">qui - 12/03 - 20:00</td>
+            </tr>
+            <tr>
+              <td>
+                <img src="img/passofundo.png">
+              </td>
+              <td align="left"  width="120px"><span>Passo Fundo</span></td>
+              <td>
+                <input type="text" size="1">
+                x
+                <input type="text" size="1">
+              </td>
+              <td align="right"  width="110px">
+                <span>Brasil de Pelotas</span>
+              </td>
+              <td>
+                <img src="img/brasilpelotas.png"> 
+              </td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</body>
+</html>
